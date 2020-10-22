93cf7cdcd24ff265d0b0f6f8e6b9bdb5e01b4e6a
matthewng1996 <e0175093@u.nus.edu>
2020-10-22 22:43:55 +0800

    For Reverse Engineering post
    For 22/10/2020 post

--- app/src/main/java/com/version/testapp/MainActivity.java
+++ app/src/main/java/com/version/testapp/MainActivity.java
@@ -3,12 +3,18 @@ package com.version.testapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        text = findViewById(R.id.text1);
         setContentView(R.layout.activity_main);
+
+        text.setText("The real Hello World!");
     }
 }
